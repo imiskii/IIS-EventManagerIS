6ea8832f7152ce1555fa8f4d4c6fff4400b2e7af
imiskii <las.michal00@gmail.com>
2023-10-06 00:42:58 +0200

    feat: FE index.php & components

---
+++ src/front-end/login.php
@@ -0,0 +1,40 @@
+<?php
+
+require "components/html-components.php";
+
+makeHead("Eventer | Log in");
+
+?>
+
+<main>
+    <div class="login-container">
+        <form action="" method="post">
+            <ul>
+                <li>
+                    <div class="input-icon">
+
+                    </div>
+                    <input type="text" name="" id="">
+                </li>
+                <li>
+                    <div class="input-icon">
+
+                    </div>
+                    <input type="text">
+                </li>
+                <li>
+                    <button></button>
+                </li>
+            </ul>
+        </form>
+    </div>
+</main>
+
+<?php
+
+makeFooter();
+
+?>
+
+</html>
+
