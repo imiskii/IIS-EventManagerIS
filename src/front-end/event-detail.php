85d5d6d843ec1534eeed45c908c750c46d5776a4
imiskii <las.michal00@gmail.com>
2023-10-09 08:16:05 +0200

    feat: login, signin page

--- src/front-end/event-detail.php
+++ src/front-end/event-detail.php
@@ -1,3 +1,34 @@
 <?php
+/**
+ * @file event-detail.php
+ * @brief page with detail information about specific event
+ * @author Michal Ľaš (xlasmi00)
+ * @date 06.08.2023
+ */
+
+
+require "components/html-components.php";
+
+makeHead("Eventer | Event Detail");
+makeHeader();
 
 ?>
+
+<main class="event-detail-main-container">
+    <div class="event-info-container">
+
+    </div>
+    <div class="tickets-container">
+
+    </div>
+    <div class="comments-container">
+
+    </div>
+</main>
+
+<?php
+
+makeFooter();
+
+?>
+
--- src/front-end/login.php
+++ src/front-end/login.php
@@ -1,32 +1,50 @@
 <?php
+/**
+ * @file login.php
+ * @brief page log in form
+ * @author Michal Ľaš (xlasmi00)
+ * @date 06.08.2023
+ */
+
 
 require "components/html-components.php";
 
-makeHead("Eventer | Log in");
+makeHead("Eventer | Sign in");
 
 ?>
 
 <main>
-    <div class="login-container">
-        <form action="" method="post">
-            <ul>
-                <li>
-                    <div class="input-icon">
-
-                    </div>
-                    <input type="text" name="" id="">
-                </li>
-                <li>
-                    <div class="input-icon">
-
-                    </div>
-                    <input type="text">
-                </li>
-                <li>
-                    <button></button>
-                </li>
-            </ul>
-        </form>
+    <div class="center-block">
+        <div class="form-container">
+            <h2>Log in</h2>
+            <form action="" method="post">
+                <ul>
+                    <li>
+                        <div class="input-row">
+                            <div class="input-icon">
+                                <i class="fa-solid fa-user"></i>
+                            </div>
+                            <input type="text" name="email" id="email" placeholder="Email">
+                        </div>
+                    </li>
+                    <li>
+                        <div class="input-row">
+                            <div class="input-icon">
+                                <i class="fa-solid fa-key"></i>
+                            </div>
+                            <input type="password" name="pwd" id="pwd" placeholder="Password">
+                        </div>
+                    </li>
+                    <li>
+                        <div class="buttons">
+                            <button class="button-round-filled" type="submit">Log in</button>
+                            <a href="#" class="button-round-empty">Create new account</a>
+                            <a href="#" class="button-round-empty"><i class="fa-solid fa-arrow-left"></i>Go back Home</a>
+                        </div>
+                    </li>
+                </ul>
+            </form>
+        </div>
     </div>
 </main>
 
