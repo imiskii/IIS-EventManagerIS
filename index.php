eaac1c034733e308147c7a6589cc4a6a2edb0bb1
alazik <alazik@redhat.com>
2023-11-23 01:05:45 +0100

    Store user values in a session

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 require 'config/common.php';
-
-session_start();
-$db = connect_to_db(); // connect to database -> returns pdo that allows us to work with the db
-
 require "src/front-end/components/html-components.php";
 
+session_start();
+$db = connect_to_db();
+
+updateSession(["categories", "locations", "min_rating", "max_rating", "date_from", "date_to"] );
 makeHead("Eventer");
 makeHeader();
 
@@ -43,13 +43,13 @@ makeHeader();
                             <li>
                                 <div class="rating-input">
                                     <label for="min-r">Min rating</label>
-                                    <input type="number" id="min-r" pattern="[0-5]" value="0" oninput="checkRatingFilterInput()" name="min_rating">
+                                    <input type="number" id="min-r" pattern="[0-5]" <?php getSessionVal("min_rating", null, 0) ?> oninput="checkRatingFilterInput()" name="min_rating">
                                 </div>
                             </li>
                             <li>
                                 <div class="rating-input">
                                     <label for="max-r">Max rating</label>
-                                    <input type="number" id="max-r" pattern="[0-5]" value="5" oninput="checkRatingFilterInput()" name="max_rating">
+                                    <input type="number" id="max-r" pattern="[0-5]" <?php getSessionVal("max_rating", null, 5) ?> oninput="checkRatingFilterInput()" name="max_rating">
                                 </div>
                             </li>
                         </ul>
@@ -58,13 +58,13 @@ makeHeader();
                 <li>
                     <div class="filter-date">
                         <label for="date-from-input">Date from:</label>
-                        <input type="date" id="date-from-input" name="date_from">
+                        <input type="date" id="date-from-input" <?php getSessionVal("date_from") ?>  name="date_from">
                     </div>
                 </li>
                 <li>
                     <div class="filter-date">
-                        <label for="date-from-input">Date to:</label>
-                        <input type="date" id="date-from-input" name="date_to">
+                        <label for="date-to-input">Date to:</label>
+                        <input type="date" id="date-to-input" <?php getSessionVal("date_to"); ?> name="date_to">
                     </div>
                 </li>
                 <li>
